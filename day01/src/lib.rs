9d22f0ce61a03f44cbe6b0260256b76dabb6622f
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-01 09:16:44 -0500

    windows() exists on vec too

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -4,7 +4,7 @@ pub fn process(input: &str, window_len: usize) -> Option<u64> {
     let mut prev = 1u64;
     let mut increases = 0u64;
 
-    for (i, window) in lines.as_slice().windows(window_len).enumerate() {
+    for (i, window) in lines.windows(window_len).enumerate() {
         let sum = window.into_iter().sum::<u64>();
 
         if i != 0 {
