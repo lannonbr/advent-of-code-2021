90891f53734493df60480cd0203effc48eab0c9c
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-02 07:02:20 -0500

    Initial setup for day02

---
+++ day02/src/main.rs
@@ -0,0 +1,41 @@
+use day02::process;
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Please specify a file as an argument",
+        ));
+    }
+
+    let input = fs::read_to_string(&args[1])?;
+
+    let pt1_result = process(&input);
+
+    println!("Result 1: {}", pt1_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        assert_eq!(
+            process(
+                "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+            ),
+            150
+        )
+    }
+}
