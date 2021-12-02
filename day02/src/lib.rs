3fac0086aaddfb4e77663c018e9575ede05f607b
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-02 17:45:03 -0500

    Finished day02

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,3 +1,53 @@
-pub fn process(input: &str) -> u32 {
-    todo!();
+pub fn process(input: &str) -> i32 {
+    let lines = input.lines();
+
+    let sections: Vec<(&str, i32)> = lines
+        .map(|line| {
+            let sub: Vec<&str> = line.split(' ').collect();
+            (sub[0], sub[1].parse::<i32>().unwrap())
+        })
+        .collect();
+
+    let mut y_pos = 0;
+    let mut z_pos = 0;
+
+    for (dir, len) in sections {
+        if dir == "forward" {
+            z_pos += len;
+        } else if dir == "up" {
+            y_pos -= len;
+        } else if dir == "down" {
+            y_pos += len;
+        }
+    }
+
+    y_pos * z_pos
+}
+
+pub fn process_pt2(input: &str) -> i32 {
+    let lines = input.lines();
+
+    let sections: Vec<(&str, i32)> = lines
+        .map(|line| {
+            let sub: Vec<&str> = line.split(' ').collect();
+            (sub[0], sub[1].parse::<i32>().unwrap())
+        })
+        .collect();
+
+    let mut y_pos = 0;
+    let mut z_pos = 0;
+    let mut aim = 0;
+
+    for (dir, len) in sections {
+        if dir == "forward" {
+            z_pos += len;
+            y_pos += aim * len;
+        } else if dir == "up" {
+            aim -= len;
+        } else if dir == "down" {
+            aim += len;
+        }
+    }
+
+    y_pos * z_pos
 }
--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,4 +1,4 @@
-use day02::process;
+use day02::{process, process_pt2};
 use std::env;
 use std::fs;
 
@@ -15,8 +15,10 @@ fn main() -> Result<(), std::io::Error> {
     let input = fs::read_to_string(&args[1])?;
 
     let pt1_result = process(&input);
+    let pt2_result = process_pt2(&input);
 
     println!("Result 1: {}", pt1_result);
+    println!("Result 2: {}", pt2_result);
     Ok(())
 }
 
@@ -36,6 +38,21 @@ down 8
 forward 2"
             ),
             150
+        )
+    }
+
+    #[test]
+    fn test_input_pt2() {
+        assert_eq!(
+            process_pt2(
+                "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+            ),
+            900
         )
     }
 }
