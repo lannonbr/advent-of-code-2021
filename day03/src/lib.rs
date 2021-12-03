b103730ad4546b4c01b0d6e7a7936b4a8b37ecdf
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-03 07:58:57 -0500

    Day3: Reduce heap alloc size by converting partition to fold
    Pt 1: 500kb alloc'd to 47kb alloc'd
    Pt 2: 256kb alloc'd to 63kb alloc'd

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -6,11 +6,16 @@ pub fn process(input: &str) -> Option<u64> {
 
     let len = &lines.iter().nth(0).unwrap().len();
     for i in 0..*len {
-        let (zeros, ones): (Vec<&str>, Vec<&str>) = lines
-            .iter()
-            .partition(|&l| l.chars().nth(i).unwrap() == '0');
+        let (zeros, ones): (i32, i32) = lines.iter().fold((0, 0), |mut acc, &x| {
+            if x.chars().nth(i).unwrap() == '0' {
+                acc.0 += 1;
+            } else {
+                acc.1 += 1;
+            }
+            acc
+        });
 
-        if zeros.len() > ones.len() {
+        if zeros > ones {
             gamma_vec.push("0");
             epsilon_vec.push("1");
         } else {
@@ -31,14 +36,25 @@ pub fn process_pt2(input: &str) -> Option<u64> {
 
     let o_len = &oxy_lines.iter().nth(0).unwrap().len();
     for i in 0..*o_len {
-        let (zeros, ones): (Vec<&str>, Vec<&str>) = oxy_lines
-            .iter()
-            .partition(|&l| l.chars().nth(i).unwrap() == '0');
+        let (zeros, ones): (i32, i32) = oxy_lines.iter().fold((0, 0), |mut acc, &x| {
+            if x.chars().nth(i).unwrap() == '0' {
+                acc.0 += 1;
+            } else {
+                acc.1 += 1;
+            }
+            acc
+        });
 
-        if zeros.len() > ones.len() {
-            oxy_lines = zeros;
+        if zeros > ones {
+            oxy_lines = oxy_lines
+                .into_iter()
+                .filter(|&l| l.chars().nth(i).unwrap() == '0')
+                .collect();
         } else {
-            oxy_lines = ones
+            oxy_lines = oxy_lines
+                .into_iter()
+                .filter(|&l| l.chars().nth(i).unwrap() == '1')
+                .collect();
         }
 
         if oxy_lines.len() == 1 {
@@ -48,14 +64,25 @@ pub fn process_pt2(input: &str) -> Option<u64> {
 
     let c_len = &co2_lines.iter().nth(0).unwrap().len();
     for i in 0..*c_len {
-        let (zeros, ones): (Vec<&str>, Vec<&str>) = co2_lines
-            .iter()
-            .partition(|&l| l.chars().nth(i).unwrap() == '0');
+        let (zeros, ones): (i32, i32) = co2_lines.iter().fold((0, 0), |mut acc, &x| {
+            if x.chars().nth(i).unwrap() == '0' {
+                acc.0 += 1;
+            } else {
+                acc.1 += 1;
+            }
+            acc
+        });
 
-        if zeros.len() > ones.len() {
-            co2_lines = ones;
+        if zeros > ones {
+            co2_lines = co2_lines
+                .into_iter()
+                .filter(|&l| l.chars().nth(i).unwrap() == '1')
+                .collect();
         } else {
-            co2_lines = zeros
+            co2_lines = co2_lines
+                .into_iter()
+                .filter(|&l| l.chars().nth(i).unwrap() == '0')
+                .collect();
         }
 
         if co2_lines.len() == 1 {
