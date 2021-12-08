e294e66ba68b7daa1d40694088e5f70c398d6b69
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-08 10:15:15 -0500

    Day 8 part 1

---
+++ day08/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn process(input: &str) -> Option<u32> {
+    let half_two: Vec<Vec<(&str, usize)>> = input.lines().map(|l| l.split('|').nth(1).unwrap().trim().split(' ').map(|s| (s, s.len())).collect()).collect();
+
+    let mut sum = 0;
+
+    for line in &half_two {
+        for (_, size) in line {
+            dbg!(size);
+            if *size == 2 || *size == 3 || *size == 4 || *size == 7 {
+                sum += 1;
+            }
+        }
+    }
+
+    Some(sum)
+}
+
+
+pub fn process_pt2(input: &str) -> Option<u32> {
+    Some(0)
+}
---
+++ day08/src/main.rs
@@ -0,0 +1,42 @@
+use day08::{process, process_pt2};
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Please specify a file as an argument",
+        ));
+    }
+
+    let input = fs::read_to_string(&args[1])?;
+
+    let pt1_result = process(&input).unwrap();
+    // let pt2_result = process_pt2(&input).unwrap();
+
+    println!("Result 1: {}", pt1_result);
+    // println!("Result 2: {}", pt2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        assert_eq!(process("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"), Some(26));
+    }
+}
