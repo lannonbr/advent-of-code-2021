087304e0a80b24711f055c828973495f339acd50
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-09 06:43:10 -0500

    Day 8 formatting

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,5 +1,16 @@
 pub fn process(input: &str) -> Option<u32> {
-    let half_two: Vec<Vec<(&str, usize)>> = input.lines().map(|l| l.split('|').nth(1).unwrap().trim().split(' ').map(|s| (s, s.len())).collect()).collect();
+    let half_two: Vec<Vec<(&str, usize)>> = input
+        .lines()
+        .map(|l| {
+            l.split('|')
+                .nth(1)
+                .unwrap()
+                .trim()
+                .split(' ')
+                .map(|s| (s, s.len()))
+                .collect()
+        })
+        .collect();
 
     let mut sum = 0;
 
@@ -14,7 +25,6 @@ pub fn process(input: &str) -> Option<u32> {
 
     Some(sum)
 }
-
 
 pub fn process_pt2(input: &str) -> Option<u32> {
     Some(0)
