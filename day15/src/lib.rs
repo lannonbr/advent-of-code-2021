f601cb1b49d60df8242ffe9c7801373e159da8bd
Benjamin Lannon <benjamin@lannonbr.com>
2021-12-15 17:17:50 -0500

    Finished day 15

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -9,12 +9,52 @@ pub fn process(input: &str) -> Option<u32> {
 
     let flat_v: Vec<u32> = v.iter().flatten().map(|c| *c).collect();
 
-    // dbg!(&v);
-
     let m = Matrix::from_vec(rows, rows, flat_v).unwrap();
 
-    // let iter = m.neighbours(false).map(|i| (i, m.get(i)));
+    solve_shortest_path(m)
+}
 
+pub fn process_pt2(input: &str) -> Option<u32> {
+    let rows = input.lines().collect::<Vec<&str>>().len();
+    let v: Vec<Vec<u32>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+
+    let mut expanded_v: Vec<Vec<u32>> = vec![vec![0; rows * 5]; rows * 5];
+
+    let add_matrix: Vec<Vec<u32>> = vec![
+        vec![0, 1, 2, 3, 4],
+        vec![1, 2, 3, 4, 5],
+        vec![2, 3, 4, 5, 6],
+        vec![3, 4, 5, 6, 7],
+        vec![4, 5, 6, 7, 8],
+    ];
+
+    for (y_idx, y) in v.iter().enumerate() {
+        for (x_idx, x) in y.iter().enumerate() {
+            expanded_v[y_idx][x_idx] = *x;
+            for y0 in 0..5 {
+                for x0 in 0..5 {
+                    expanded_v[(y_idx + y0 * rows) as usize][(x_idx + x0 * rows) as usize] =
+                        if x + add_matrix[y0][x0] > 9 {
+                            1 + ((x + add_matrix[y0][x0]) % 10)
+                        } else {
+                            x + add_matrix[y0][x0]
+                        };
+                }
+            }
+        }
+    }
+
+    let flat_v: Vec<u32> = expanded_v.iter().flatten().map(|c| *c).collect();
+
+    let m = Matrix::from_vec(rows * 5, rows * 5, flat_v).unwrap();
+
+    solve_shortest_path(m)
+}
+
+fn solve_shortest_path(m: Matrix<u32>) -> Option<u32> {
     let result = dijkstra(
         &(0, 0),
         |p| m.neighbours(*p, false).map(|p| (p, *m.get(p).unwrap())),
@@ -22,14 +62,10 @@ pub fn process(input: &str) -> Option<u32> {
     );
 
     if result.is_some() {
-        let (path, weight) = result.unwrap();
+        let (_, weight) = result.unwrap();
 
         return Some(weight);
     }
 
     None
-}
-
-pub fn process_pt2(input: &str) -> Option<u32> {
-    Some(5)
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), std::io::Error> {
     let input = fs::read_to_string(&args[1])?;
 
     let pt1_result = process(&input).unwrap();
-    // let pt2_result = process_pt2(&input).unwrap();
+    let pt2_result = process_pt2(&input).unwrap();
 
     println!("Result 1: {}", pt1_result);
-    // println!("Result 2: {}", pt2_result);
+    println!("Result 2: {}", pt2_result);
     Ok(())
 }
 
@@ -42,6 +42,25 @@ mod tests {
 2311944581"
             ),
             Some(40)
+        );
+    }
+
+    #[test]
+    fn test_input_pt2() {
+        assert_eq!(
+            process_pt2(
+                "1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581"
+            ),
+            Some(315)
         );
     }
 }
